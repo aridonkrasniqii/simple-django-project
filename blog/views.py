fa5550c2eb175ba7fb81d9a0f4da5bec35572548
aridonkrasniqii <aridonk16@outlook.com>
2022-10-04 21:33:48 +0200

    first commit

---
+++ blog/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+"""def home(request):
+    return HttpResponse('<h1>Blog Home </h1>')
+"""
+"""
+def about(request):
+    return HttpResponse("<h1> Blog About </h1>")
+"""
+
+
+
+
+posts = [
+
+    {
+        'author':'CoreyMS',
+        'title': 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted':'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
+
+def about(request):
+    return render(request, 'blog/about.html', context= {'title' : 'Author'})
+
+def article(request):
+
+    return render(request, 'blog/article.html', context={})
